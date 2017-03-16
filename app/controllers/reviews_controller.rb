03a89f6d75bae3c49eb97fa775069175dc4d83e0
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-03-15 21:07:53 -0400

    add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,58 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def edit
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.json { render :show, status: :created, location: @review }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @review.update(review_params)
+        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.json { render :show, status: :ok, location: @review }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @review.destroy
+    respond_to do |format|
+      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+    end
+  end
+
+  private
+
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :comment, :user_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reviews
   root to: 'visitors#index'
   devise_for :users, :controllers => { registrations: 'registrations'}
 
