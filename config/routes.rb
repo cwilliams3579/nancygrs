fb80673be364f3a98b2acde5ff4f77f3e7903c4f
cwillliams3579 <clevelandwilliams3579@gmail.com>
2017-03-26 11:06:02 -0500

    add dropdown to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations'}
+  # scope ":locale", locale: /#{I18n.available_locales.join["|"]}/ do
+    devise_for :users, :controllers => { registrations: 'registrations'}
 
-  resources :posts do
-    resources :comments, only: [:create]
-  end
-  resources :testimonials
-
-  resources :exercises do
-    member do
-      put "like", to: "exercises#upvote"
-      put "dislike", to: "exercises#downvote"
+    resources :posts do
+      resources :comments, only: [:create]
     end
-    resources :reviews, except: [:show, :index]
-  end
-  root to: 'visitors#index'
-  mount ActionCable.server => '/cable'
+    resources :testimonials
+
+    resources :exercises do
+      member do
+        put "like", to: "exercises#upvote"
+        put "dislike", to: "exercises#downvote"
+      end
+      resources :reviews, except: [:show, :index]
+    end
+    root to: 'visitors#index'
+    mount ActionCable.server => '/cable'
+  # end
 end
