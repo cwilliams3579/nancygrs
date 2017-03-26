ab9063f81efab72c3cfb33c36ab370f19f630640
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-03-26 12:49:40 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,24 @@
 Rails.application.routes.draw do
   # scope ":locale", locale: /#{I18n.available_locales.join["|"]}/ do
-    devise_for :users, :controllers => { registrations: 'registrations'}
+  devise_for :users, :controllers => { registrations: 'registrations'}
 
-<<<<<<< HEAD
   resources :posts do
     resources :comments, only: [:create]
   end
-
   resources :testimonials
-=======
-    resources :posts do
-      resources :comments, only: [:create]
-    end
-    resources :testimonials
->>>>>>> fb80673be364f3a98b2acde5ff4f77f3e7903c4f
 
-    resources :exercises do
-      member do
-        put "like", to: "exercises#upvote"
-        put "dislike", to: "exercises#downvote"
-      end
-      resources :reviews, except: [:show, :index]
+  resources :exercises do
+    member do
+      put "like", to: "exercises#upvote"
+      put "dislike", to: "exercises#downvote"
     end
-<<<<<<< HEAD
     resources :reviews, except: [:show, :index]
   end
 
-  resources :contacts, only: [:new, :create] 
+resources :contacts, only: [:new, :create]
 
-  root to: 'visitors#index'
-  mount ActionCable.server => '/cable'
-=======
-    root to: 'visitors#index'
-    mount ActionCable.server => '/cable'
-  # end
->>>>>>> fb80673be364f3a98b2acde5ff4f77f3e7903c4f
+root to: 'visitors#index'
+mount ActionCable.server => '/cable'
+
+# end
 end
