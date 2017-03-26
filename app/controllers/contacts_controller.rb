7a7df4384b47eb9a90e59a1e618f133a59ebf252
cwillliams3579 <clevelandwilliams3579@gmail.com>
2017-03-26 18:15:42 -0500

    update contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,13 +4,20 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      flash[:notice] = 'Thank you for your message. We will contact you soon!'
+      redirect_to root_path
     else
       flash.now[:danger] = 'Cannot send message.'
       render :new
     end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :number, :message, :nickname)
   end
 end
