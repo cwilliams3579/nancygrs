08eda8c608dfce8bfba40a6da443c0e4cb3155f1
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-04-02 20:49:57 -0400

    add missing image to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326201836) do
+ActiveRecord::Schema.define(version: 20170403001332) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170326201836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "image"
   end
 
   create_table "reviews", force: :cascade do |t|
